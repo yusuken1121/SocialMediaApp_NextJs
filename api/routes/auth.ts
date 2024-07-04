4e5ece51d7765dd103691dbb2cbc29883dfff5ad
yusuken1121 <yusuken1121@gmail.com>
2024-07-03 23:04:38 -0700

    ♻️ Split the routing modules

---
+++ api/routes/auth.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+const prisma = new PrismaClient();
+
+//API for register a new user
+router.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashPassword,
+    },
+  });
+});
+
+//API for login
+router.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res.status(401).json({ error: "The user does not exist" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "Invalid password" });
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY!, {
+    expiresIn: "1d",
+  });
+  return res.json({ token });
+});
+
+export default router;
--- api/server.ts
+++ api/server.ts
@@ -1,47 +1,10 @@
-import express, { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import express from "express";
 import "dotenv/config";
+import authRouter from "./routes/auth";
 
 const app = express();
 const PORT = 5001;
 
-const prisma = new PrismaClient();
-
 app.use(express.json());
-
-//API for register a new user
-app.post("/api/auth/register", async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashPassword = await bcrypt.hash(password, 10);
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashPassword,
-    },
-  });
-});
-
-//API for login
-app.post("/api/auth/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await prisma.user.findUnique({ where: { email } });
-
-  if (!user) {
-    return res.status(401).json({ error: "The user does not exist" });
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    return res.status(401).json({ error: "Invalid password" });
-  }
-
-  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY!, {
-    expiresIn: "1d",
-  });
-  return res.json({ token });
-});
-
+app.use("/api/auth", authRouter);
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
