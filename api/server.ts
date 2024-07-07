d6751fe32c236a20eea896c2ce7e1ba55fd80485
yusuken1121 <yusuken1121@gmail.com>
2024-07-07 14:50:26 -0700

    ✨ Add morgan

--- api/server.ts
+++ api/server.ts
@@ -3,12 +3,13 @@ import "dotenv/config";
 import authRouter from "./routes/auth";
 import postRouter from "./routes/posts";
 import cors from "cors";
-
+import morgan from "morgan";
 const app = express();
 const PORT = 5001;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
 app.use("/auth", authRouter);
 app.use("/posts", postRouter);
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
