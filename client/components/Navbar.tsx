62c34f68a4efeeee9cd63fc38b77f589cebed458
yusuken1121 <yusuken1121@gmail.com>
2024-07-07 21:59:53 -0700

    ✨ Modify navbar display based on user state

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,7 +1,10 @@
+"use client";
+import { AuthCtx } from "@/context/Auth";
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 
 const Navbar = () => {
+  const { user, login } = useContext(AuthCtx);
   return (
     <header className="bg-gray-700 p-4 text-white">
       <div className="container mx-auto flex justify-between items-center">
@@ -10,20 +13,34 @@ const Navbar = () => {
         </h1>
         <nav>
           <ul className="flex space-x-4">
-            <>
-              <Link
-                href="/login"
-                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
-              >
-                Login
-              </Link>
-              <Link
-                href="/signup"
-                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
-              >
-                Sign up
-              </Link>
-            </>
+            {user ? (
+              <>
+                <Link
+                  href={`/profile/${user.id}`}
+                  className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                >
+                  Profile
+                </Link>
+                <button className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium">
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/signup"
+                  className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                >
+                  Sign up
+                </Link>
+              </>
+            )}
           </ul>
         </nav>
       </div>
