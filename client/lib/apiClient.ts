0b52ddf0cb8ed6bdea62b4ba70bffffa438d2342
yusuken1121 <yusuken1121@gmail.com>
2024-07-11 08:11:40 -0700

    🚀 Change the hostURL in frontend

--- client/lib/apiClient.ts
+++ client/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5001", // API server
+  // baseURL: "http://localhost:5001", // API server
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
