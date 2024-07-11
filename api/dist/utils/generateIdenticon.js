31d1e8c36483d380993495b385c1d06142001dc7
yusuken1121 <yusuken1121@gmail.com>
2024-07-11 07:54:25 -0700

    🚀 Reorganize the dependencies and package.json

--- api/dist/utils/generateIdenticon.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateAvatar = void 0;
-const colors = ["FF548F", "9061C2", "BE80FF", "63D3FF", "02779E"];
-const lowerStrColors = colors.map((color) => color.toLocaleLowerCase());
-const generateAvatar = (name) => {
-    return `https://source.boringavatars.com/beam/40/${encodeURIComponent(name)}?colors=${lowerStrColors.join(",")}`;
-};
-exports.generateAvatar = generateAvatar;
