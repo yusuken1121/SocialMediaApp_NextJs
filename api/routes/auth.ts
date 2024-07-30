919d4021484365c37d09182a3741a3823327e3aa
yusuken1121 <yusuken1121@gmail.com>
2024-07-29 19:40:42 -0700

    🐛 Deleted the unnecessary lines.

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
 import { login, register } from "../controllers/auth.controller";
 
 const router = express.Router();
-
-const prisma = new PrismaClient();
 
 //API for register a new user
 router.post("/register", register);
